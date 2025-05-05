22f3620f14244bf2099290af50da6dfff5c1043b
Till Bungert <tillbungert@gmail.com>
2025-05-05 11:24:40 +0200

    feat: wordlist to embedding vectors

---
+++ solvtext/data.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+import numpy as np
+import numpy.typing as npt
+
+
+def load_words() -> tuple[list[str], npt.NDArray[np.float32]]:
+    data_path = Path("/home/tillb/Projects/context-me-solver/data")
+
+    if (data_path / "filtered_words.txt").is_file() and (data_path / "vectors.npz").is_file():
+        with (data_path / "filtered_words.txt").open() as f:
+            filtered_words = [word.strip() for word in f.readlines()]
+
+        vectors = np.load(data_path / "vectors.npz")
+
+        return filtered_words, vectors["arr_0"]
+
+    glove: dict[str, npt.ArrayLike] = {}
+
+    with open("./data/glove.840B.300d.txt", "rt") as f:
+        for line in f:
+            entries = line.split(" ")
+            glove[entries[0]] = np.array(entries[1:], dtype=np.float32)
+
+    with open("./data/google-10000-english-usa-no-swears.txt", "rt") as f:
+        words = [word.strip() for word in f.readlines()]
+
+    filtered_words = [word for word in words if word in glove]
+    vectors = np.vstack([glove[word] for word in filtered_words])
+
+    with open("./data/filtered_words.txt", "w") as f:
+        f.write("\n".join(filtered_words))
+
+    np.savez_compressed("./data/vectors.npz", vectors)
+
+    return filtered_words, vectors
