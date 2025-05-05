0f529a368d280e388967ea0aff75079b7265a045
Till Bungert <tillbungert@gmail.com>
2025-05-05 11:19:27 +0200

    feat: download wordlist and embeddings

---
+++ prepare-data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+wget -P data 'https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/google-10000-english-usa-no-swears.txt'
+wget -P data 'https://nlp.stanford.edu/data/glove.840B.300d.zip'
