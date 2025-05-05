fdd91cebfe840f9f3bf31c123f89b87d523e85ba
Till Bungert <tillbungert@gmail.com>
2025-05-05 12:25:33 +0200

    feat: add simulated games for debugging

--- solvtext/solve.py
+++ solvtext/solve.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Literal, overload
 from solvtext.data import load_words
 from functools import lru_cache
 from rich.prompt import IntPrompt
@@ -47,13 +48,25 @@ class Solver:
                 other.word_idx
             )
 
-    def make_guess(self) -> int | None:
+    @overload
+    def make_guess(self, interactive: Literal[False]) -> int: ...
+
+    @overload
+    def make_guess(self, interactive: Literal[True]) -> None: ...
+
+    @overload
+    def make_guess(self) -> None: ...
+
+    def make_guess(self, interactive: bool = True) -> int | None:
         print(self.candidate_mask.sum())
 
         word_idx = np.random.choice(np.where(self.candidate_mask)[0])
         word = self.words[word_idx]
 
         self.candidate_mask[word_idx] = 0
+
+        if not interactive:
+            return word_idx
 
         rank = IntPrompt.ask(f"Guess: {word}\nEnter rank (or -1 if word is unknown)")
         if rank == -1:
@@ -62,12 +75,41 @@ class Solver:
         self.add_guess(word_idx, rank)
 
 
-def main():
+def simulate():
     solver = Solver()
+    target_idx = np.random.choice(np.arange(len(solver.words)))
+
+    distances = solver.distances(target_idx).copy()
+    rank = distances.argsort().argsort()
+
     num_guesses = 0
-    try:
-        while True:
-            solver.make_guess()
-            num_guesses += 1
-    except KeyboardInterrupt:
-        print(f"{num_guesses}")
+
+    print(solver.words[target_idx])
+
+    while True:
+        guess_idx = solver.make_guess(interactive=False)
+        solver.add_guess(guess_idx, rank[guess_idx])
+
+        num_guesses += 1
+
+        if rank[guess_idx] == 0:
+            break
+
+    return num_guesses
+
+
+def main():
+    num_guesses = []
+    for _ in range(100):
+        num_guesses.append(simulate())
+
+    print(np.mean(num_guesses))
+
+    # solver = Solver()
+    # num_guesses = 0
+    # try:
+    #     while True:
+    #         solver.make_guess()
+    #         num_guesses += 1
+    # except KeyboardInterrupt:
+    #     print(f"{num_guesses}")
