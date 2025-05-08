eb8ec3d7c82c6d10a0feb8058ad952d7e1c2de83
Till Bungert <tillbungert@gmail.com>
2025-05-09 00:39:53 +0200

    feat: download word list and glove embeddings

--- solvtext/data.py
+++ solvtext/data.py
@@ -1,14 +1,36 @@
 from pathlib import Path
+from zipfile import ZipFile
+
+import nltk
 import numpy as np
 import numpy.typing as npt
-import nltk
+import requests
 from nltk.stem import WordNetLemmatizer
+from rich.progress import track
+
+
+def _download(file_path: Path, url: str):
+    if file_path.is_file():
+        return
+
+    try:
+        with file_path.open("wb") as f:
+            with requests.get(url, stream=True) as r:
+                r.raise_for_status()
+                for chunk in track(
+                    r.iter_content(chunk_size=8192),
+                    total=int(r.headers.get("content-length", 0)) / 8192,
+                ):
+                    f.write(chunk)
+    except:
+        file_path.unlink()
+        raise
 
 
 def load_words(data_dir: Path) -> tuple[list[str], npt.NDArray[np.float32]]:
-    # data_dir = Path("/home/tillb/Projects/context-me-solver/data")
+    data_dir.mkdir(exist_ok=True)
+
     embedding_file = "glove.840B.300d"
-    # embedding_file = "glove.42B.300d"
 
     if (data_dir / "words.txt").is_file() and (data_dir / "vectors.npz").is_file():
         with (data_dir / "words.txt").open() as f:
@@ -26,6 +48,12 @@ def load_words(data_dir: Path) -> tuple[list[str], npt.NDArray[np.float32]]:
             "arr_0"
         ].item()
     else:
+        _download(
+            data_dir / f"{embedding_file}.zip",
+            "https://nlp.stanford.edu/data/glove.840B.300d.zip",
+        )
+        with ZipFile(data_dir / f"{embedding_file}.zip") as f:
+            f.extractall(data_dir)
         with (data_dir / f"{embedding_file}.txt").open() as f:
             for line in f:
                 entries = line.split(" ")
@@ -36,13 +64,17 @@ def load_words(data_dir: Path) -> tuple[list[str], npt.NDArray[np.float32]]:
         )
 
     print("Encoding word list...")
-    wnl = WordNetLemmatizer()
 
+    _download(
+        data_dir / "20k.txt",
+        "https://raw.githubusercontent.com/first20hours/google-10000-english/refs/heads/master/20k.txt",
+    )
     with (data_dir / "20k.txt").open() as f:
         words = {word.strip() for word in f.readlines()}
 
-    # not necessarily correct from a nlp perspective, but contexto.me seems to treat all infinitives this way,
-    # e.g. shipping and ship are assigned the same rank
+    # not necessarily correct, but contexto.me seems to try to lemmatize all inputs
+    nltk.download("wordnet")
+    wnl = WordNetLemmatizer()
     words = {
         wnl.lemmatize(word, pos="v" if word.endswith("ing") else "n") for word in words
     }
